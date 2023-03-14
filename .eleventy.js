39cc999ed719fadcdeb6da2f26ece144a6768975
Tom-Mai <88791693+Tom-Mai@users.noreply.github.com>
2023-03-14 13:22:43 +0100

    change output to _site + news line for optimazd img

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,53 @@
+// imports for the various eleventy plugins (navigation & image)
+const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
+const { DateTime } = require('luxon');
+const Image = require('@11ty/eleventy-img');
+const path = require('path');
+
+// allows the use of {% image... %} to create responsive, optimised images
+// CHANGE DEFAULT MEDIA QUERIES AND WIDTHS
+async function imageShortcode(src, alt, className, loading, sizes = '(max-width: 600px) 400px, 850px') {
+  // don't pass an alt? chuck it out. passing an empty string is okay though
+  if (alt === undefined) {
+    throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`);
+  }
+
+  // create the metadata for an optimised image
+  let metadata = await Image(`${src}`, {
+    widths: [200, 400, 850, 1920, 2500],
+    formats: ['webp', 'jpeg'],
+    urlPath: '/images/',
+    outputDir: './public/images',
+    filenameFormat: function (id, src, width, format, options) {
+      const extension = path.extname(src);
+      const name = path.basename(src, extension);
+      return `${name}-${width}w.${format}`;
+    },
+  });
+
+  // get the smallest and biggest image for picture/image attributes
+  let lowsrc = metadata.jpeg[0];
+  let highsrc = metadata.jpeg[metadata.jpeg.length - 1];
+
+  // when {% image ... %} is used, this is what's returned
+  return `<picture class="${className}">
+    ${Object.values(metadata)
+      .map((imageFormat) => {
+        return `  <source type="${imageFormat[0].sourceType}" srcset="${imageFormat
+          .map((entry) => entry.srcset)
+          .join(', ')}" sizes="${sizes}">`;
+      })
+      .join('\n')}
+      <img
+        src="${lowsrc.url}"
+        width="${highsrc.width}"
+        height="${highsrc.height}"
+        alt="${alt}"
+        loading="${loading}"
+        decoding="async">
+    </picture>`;
+}
+
 /* Config file */
 module.exports = function(eleventyConfig) {
 
@@ -12,12 +62,8 @@ module.exports = function(eleventyConfig) {
       dir: {
         // Define the input & output folder
           input: "src",
-          includes: '_includes',
-          layouts: "_layouts",
           output: "_site"
       },
-          // allows .html files to contain nunjucks templating language
-    htmlTemplateEngine: 'njk',
     };
 };
 
