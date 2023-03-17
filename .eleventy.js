dbe6253778aad629e015e918ca012f6065d5c71d
Tom-Mai <88791693+Tom-Mai@users.noreply.github.com>
2023-03-17 14:52:10 +0100

    still trying to fix

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,8 @@ module.exports = function(eleventyConfig) {
     // Clone the config object in public folder
     eleventyConfig.addPassthroughCopy("./src/assets");
     eleventyConfig.addPassthroughCopy('./src/admin');
-    eleventyConfig.addPassthroughCopy('._site/images/uploads');
+    eleventyConfig.addPassthroughCopy({"./src/images": "/images"});
+
 
     return {
       dir: {
