101329dd30042da63172419cb4a1af94a3339692
Tom-Mai <88791693+Tom-Mai@users.noreply.github.com>
2023-03-17 14:49:17 +0100

    trying to fix _site

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,9 @@
-/* Config file */
 module.exports = function(eleventyConfig) {
 
     // Clone the config object in public folder
-
     eleventyConfig.addPassthroughCopy("./src/assets");
     eleventyConfig.addPassthroughCopy('./src/admin');
+    eleventyConfig.addPassthroughCopy('._site/images/uploads');
 
     return {
       dir: {
@@ -14,4 +13,3 @@ module.exports = function(eleventyConfig) {
       },
     };
 };
-
