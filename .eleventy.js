21c3995d7865aeb4334d947965cf621b962556d7
Tom-Mai <88791693+Tom-Mai@users.noreply.github.com>
2023-03-20 13:45:38 +0100

    delete line 6 useless

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,7 @@ module.exports = function(eleventyConfig) {
     // Clone the config object in public folder
     eleventyConfig.addPassthroughCopy("./src/assets");
     eleventyConfig.addPassthroughCopy('./src/admin');
-    eleventyConfig.addPassthroughCopy({"./_site/images": "/images"});
+    // eleventyConfig.addPassthroughCopy({"./_site/images": "/images"});
 
 
     return {
