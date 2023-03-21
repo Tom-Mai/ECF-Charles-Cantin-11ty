3eb8e280970b4a5568cf81d54be7e4a788d34d38
Tom-Mai <88791693+Tom-Mai@users.noreply.github.com>
2023-03-21 02:37:09 +0100

    add enlarge script

--- src/assets/js/portfolio.js
+++ src/assets/js/portfolio.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
+  
     const filterButtons = document.querySelectorAll(".filter-btn");
     const items = document.querySelectorAll(".col-photo");
-  
+    
+
     filterButtons.forEach((button) => {
       button.addEventListener("click", () => {
         const category = button.dataset.id;
@@ -15,5 +17,14 @@ document.addEventListener("DOMContentLoaded", () => {
         });
       });
     });
+    document.querySelectorAll('.container img').forEach(image => {
+      image.onclick = () => {
+        document.querySelector('.enlarge').style.display = "block";
+        document.querySelector('.enlarge img').src = image.getAttribute('src');
+      }
+    });
+    document.querySelector('.enlarge span').onclick = () => {
+      document.querySelector('.enlarge').style.display = "none";
+    };
   });
-  
+ 
