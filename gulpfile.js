db8b57d9a6171600a80abae1d5e12536ac348da6
Tom-Mai <88791693+Tom-Mai@users.noreply.github.com>
2023-06-19 08:06:28 +0200

    add script for optimiz gulpfile --in progress--

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var cleanCSS = require('gulp-clean-css');
+
+function minifyCss() {
+    return gulp.src('src/assets/css/*.css')
+    .on('data', function(file) {
+        console.log('Found file', file.path);
+    })
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('public/assets/css/'))
+    .on('end', function() {
+        console.log('CSS minification done.');
+    });
+}
+
+gulp.task('minify-css', minifyCss);
+
+function watch() {
+    gulp.watch('src/assets/css/*.css', minifyCss);
+}
+gulp.task('watch', watch);
+
+gulp.task('default', watch);
