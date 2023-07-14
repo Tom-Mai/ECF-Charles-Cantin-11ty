2c25791f2befaa992ed5a4f479c5db686bc50fff
Tom Ma <mailleux.tom@gmail.com>
2023-07-14 13:22:47 +0200

    fix the clientwidth maj

--- src/assets/js/price.js
+++ src/assets/js/price.js
@@ -7,17 +7,17 @@ onload = () => {
 }
 
 function displayData() {
- 
-  // Récupération de la largeur de chaque slide après qu'ils ont été ajoutés au DOM
-  const slide = document.querySelector('.slide');
-  const slideWidth = slide.clientWidth;
-
   // Ajout des écouteurs d'événements pour les boutons prev/next
   nextButton.addEventListener("click", ()=>{
+      const slide = document.querySelector('.slide');
+      const slideWidth = slide.clientWidth;
       slideContainer.scrollLeft += slideWidth;
   });
   prevButton.addEventListener("click", () => {
+      const slide = document.querySelector('.slide');
+      const slideWidth = slide.clientWidth;
       slideContainer.scrollLeft -= slideWidth;
   });
 };
+
 
